6a626801c9e707bccdaf1a6cadd88546448ebb59
Bosko Lazic <boskolazicbl@gmail.com>
2023-01-23 17:38:16 +0100

    Added another level of abstraction in service

---
+++ VacationTracker/src/main/java/project/service/impl/JpaAdminService.java
@@ -0,0 +1,72 @@
+package project.service.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import project.model.UsedVacation;
+import project.model.UserEntity;
+import project.model.Vacation;
+import project.repository.UsedVacationRepository;
+import project.repository.UserRepository;
+import project.repository.VacationDaysRepository;
+import project.service.AdminService;
+import project.util.CSVUtil;
+
+@Service
+public class JpaAdminService implements AdminService{
+ 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private UsedVacationRepository usedVacationRepository;
+	
+	@Autowired
+	private VacationDaysRepository vacationDaysRepository;
+	
+	public void saveEmployees(MultipartFile file, PasswordEncoder passwordEncoder) {
+		try {
+			CSVUtil csvUtil = new CSVUtil();
+			
+			List<UserEntity> users = csvUtil.csvToUsers(file.getInputStream(), passwordEncoder);
+			userRepository.saveAll(users);
+		} catch (IOException e) {
+			throw new RuntimeException("fail to store csv data: " + e.getMessage());
+		}
+	}
+	
+	public void saveUsedVacation(MultipartFile file) {
+		try {
+			CSVUtil csvUtil = new CSVUtil();
+			
+			List<UsedVacation> usedVacation = csvUtil.csvToUsedVacation(file.getInputStream(), userRepository);
+			usedVacationRepository.saveAll(usedVacation);
+		} catch (IOException e) {
+			throw new RuntimeException("fail to store csv data: " + e.getMessage());
+		}
+	}
+	
+	public void saveTotalVacationDays(MultipartFile file) {
+		try {
+			CSVUtil csvUtil = new CSVUtil();
+			
+			List<Vacation> totalVacationDays = csvUtil.csvToTotalVacationDays(file.getInputStream(), userRepository);
+			vacationDaysRepository.saveAll(totalVacationDays);
+		} catch (IOException e) {
+			throw new RuntimeException("fail to store csv data: " + e.getMessage());
+		}
+	}
+
+	public List<UserEntity> getAllUsers() {
+		return userRepository.findAll();
+	}
+	
+	public List<UsedVacation> getAllVacation(){
+		return usedVacationRepository.findAll();
+	}
+}
